6ed269d36d42cc9e674dcb17ce3a037792acf8e4
Joao Vitor Santos Bezerra <joaovitor.santos@e-core.com>
2023-04-26 01:36:08 -0300

    add features  <br>

---
+++ webtron/webtron.py
@@ -0,0 +1,27 @@
+import boto3
+import click
+
+session = boto3.Session(profile_name='solvimm')
+s3 = session.resource('s3')
+
+@click.group() # cria grupo
+def cli():
+    "webtron deploys websites to AWS"
+    pass
+
+@cli.command('list-buckets') # adiciona comando dentro do grupo
+def list_buckets(): # comando propriamente dito
+    "List all s3 buckets"
+    for bucket in s3.buckets.all():
+        print(bucket)
+
+
+@cli.command('list-bucket-objects')
+@click.argument('bctname')
+def list_bucket_objects(bctname):
+    "List objects in an s3 bucket"
+    for obj in s3.Bucket(bctname).objects.all(): #percorre e printa os arquivos
+        print(obj)
+
+if __name__ == '__main__':
+    cli()
